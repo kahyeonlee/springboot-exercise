27cc44e382533b9256619047785f0229e5234a6b
kahyun Lee <gmeoqq@gmail.com>
2022-10-28 15:45:22 +0900

    add 기능추가

--- src/main/java/com/springbooot/springboot_exercise/controller/UserController.java
+++ src/main/java/com/springbooot/springboot_exercise/controller/UserController.java
@@ -2,10 +2,10 @@ package com.springbooot.springboot_exercise.controller;
 
 
 import com.springbooot.springboot_exercise.dao.UserDao;
+import com.springbooot.springboot_exercise.domain.User;
+import com.springbooot.springboot_exercise.domain.dto.UserRequestDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,7 +15,17 @@ public class UserController {
     public UserController(UserDao userDao) {
         this.userDao = userDao;
     }
-
+//    @GetMapping("/user")
+//    public ResponseEntity<User> get(@PathVariable String id){
+//        return this.userDao.deleteById(id);
+//    }
+    @PostMapping("/user")
+    public ResponseEntity<Integer> add(@RequestBody UserRequestDto userRequestDto) {
+        User user = new User(userRequestDto.getId(),userRequestDto.getName(),userRequestDto.getPassword());
+        return ResponseEntity
+                .ok()
+                .body(userDao.deleteAll());
+    }
     @DeleteMapping("/user")
     public ResponseEntity<Integer> deleteAll() {
         return ResponseEntity
@@ -23,6 +33,5 @@ public class UserController {
                 .body(userDao.deleteAll());
     }
 
-    
-
 }
+
