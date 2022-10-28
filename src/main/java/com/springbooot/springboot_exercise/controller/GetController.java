b924981135c4ac83a6f26241480225fa8710fa03
kahyun Lee <gmeoqq@gmail.com>
2022-10-28 11:33:35 +0900

    로깅 만들어보기

--- src/main/java/com/springbooot/springboot_exercise/controller/GetController.java
+++ src/main/java/com/springbooot/springboot_exercise/controller/GetController.java
@@ -1,26 +1,33 @@
 package com.springbooot.springboot_exercise.controller;
 
 import com.springbooot.springboot_exercise.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/vi/get-api")
+@Slf4j
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(){
+    public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
+
     }
 
     @GetMapping(value = "/name")
     public String getName(){
+        log.info("getName으로 요청이 들어왔습니다.");
         return "gahyun";
     }
 
     @GetMapping(value = "/variable1/{variable}")
-    public String getVariable(@PathVariable String variable){
+    public String getVariable(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다.variable:{}",variable);
         return variable;
+
     }
 
     @GetMapping(value = "/request1")
